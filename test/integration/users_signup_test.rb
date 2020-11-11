05a055dc724f90a052ba1b4c7b13c14d5537cf41
yuki takagi <yuki99takagi@gmail.com>
2020-11-12 08:59:02 +0900

    update Error messages

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_select 'ul' do
+      assert_select 'li', 'Name can\'t be blank'
+      assert_select 'li', 'Email is invalid'
+      assert_select 'li', 'Password confirmation doesn\'t match Password'
+    end
+  end
+end
